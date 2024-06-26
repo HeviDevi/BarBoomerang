680a10111b82d8139d09380ada71d72ca774f64e
HeviDevi <devon.r.ross@gmail.com>
2024-06-26 12:28:00 -0400

    set up basic MongoDB connection using .env package

---
+++ server.js
@@ -0,0 +1,25 @@
+const { MongoClient } = require('mongodb')
+express = require('express')
+require('dotenv').config()
+app = express()
+PORT = 3001
+URI = process.env.DB_URI
+client = new MongoClient(URI)
+let db;
+const dbName = process.env.DB_NAME
+
+
+client.connect()
+    .then(() => {
+        console.log('Connected to MongoDB')
+        db = client.db(dbName)
+
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`)
+        })
+    })
+    .catch(err => {
+        console.error(err)
+    })
+
+    app.use(express.json());
