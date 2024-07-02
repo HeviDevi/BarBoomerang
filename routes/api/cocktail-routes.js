09665ed09cc693bb1a18879d8e620c39a05e2ece
HeviDevi <devon.r.ross@gmail.com>
2024-07-02 18:47:33 -0400

    added get, getById, & post routes to cocktail-routes.js

--- routes/api/cocktail-routes.js
+++ routes/api/cocktail-routes.js
@@ -1,15 +1,50 @@
 const router = require("express").Router();
 const  Cocktail  = require("../../models/Cocktail");
+const User = require("../../models/User");
 
 //CRUD operations for cocktail
 
-router.post("/", (req, res) => {
-  res.send("New Cocktail Added");
+router.post("/:id", (req, res) => {
+
+  // Ensure the request body looks like this
+  //   {
+  //     "cocktailName": "intended cocktail name",
+  //     "instructions": "intended instructions",
+  //   },
+  // Also include the user ID in the URL
+
+
+  // We create a new cocktail by combining the request body with the user ID
+  const newCocktailData = {
+    ...req.body,
+    userID: req.params.id,
+  };
+  Cocktail.create(newCocktailData)
+  .then(data => { 
+    return User.updateOne(
+        { _id: data.userID }, 
+        { $push: { cocktails: data._id } }
+      )
+  })
+  .then(data => {
+    res.json(data);
+  });
 });
 
 router.get("/", (req, res) => {
-  res.send("All Cocktails");
+  Cocktail.find({})
+  .exec()
+  .then(data => {
+    res.json(data);
+  });
 });
+
+router.get("/:id", (req, res) => {
+  Cocktail.findOne({_id: req.params.id})
+  .then(data => {
+    res.json(data);
+  })
+})
 
 router.put("/", (req, res) => {
   res.send("Cocktail Updated");
